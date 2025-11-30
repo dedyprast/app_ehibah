85965ccfb0e4e1acf06801e9b264e0e9dd394d69
dedyprast <kuliah.didiet@gmail.com>
2025-11-30 21:43:33 +0700

    pertemuan 3

---
+++ lib/widgets/football_player_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+// Stateless hanya akan menampilkan, akan berubah jika statefull mengalami build ulang
+// Fungsi stateless hanya untuk menampilkan UI
+// set state hanya bisa di dtateful widget
+
+class FootballPlayerWidget extends StatelessWidget {
+  final String name;
+  final String country;
+  final String? imageUrl;
+  final bool isFavorite;
+  final Function()? onTap;
+
+  const FootballPlayerWidget({
+    super.key,
+    required this.name,
+    required this.country,
+    this.imageUrl,
+    this.isFavorite = false,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap?.call,
+      child: Card(
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            spacing: 16,
+            children: [
+              CircleAvatar(
+                foregroundImage: imageUrl != null
+                    ? NetworkImage(imageUrl!)
+                    : null,
+                child: Text(name.isNotEmpty ? name[0] : "?"),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      name,
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(country, style: TextStyle(fontSize: 12)),
+                  ],
+                ),
+              ),
+              if (isFavorite) Icon(Icons.star, color: Colors.amber),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
